40e70cbd49fcd2ef7c53cccfac2540e5820e11fd
陈昱竹 <826537618@qq.com>
2017-08-20 15:19:29 +0800

    reconstruct

--- main.js
+++ main.js
@@ -2,47 +2,43 @@
  * Created by cyz on 2017/8/20.
  */
 'use strict';
-function find(collection,element){
-    for(let item of collection){
-        if(item['key']== element){
-            return item
-        }
-    }
-    return false
-}
 
 function countLetters(str){
-    let result =[];
-    for(let i=0;i<str.length;i++){
-        let finded = find(result,str.charAt(i))
-        if(finded){
-            finded.count+=1;
+    let result ={};
+    for(let item of str){
+
+        if(result[item]){
+            result[item]++;
         }else{
-            result.push({key:str.charAt(i),count:1})
+            result[item]=1;
         }
     }
     return result;
+
 }
 
-function getMaxCount(collection){
-    let result =[];
-    collection.forEach((item)=>{
-        result.push(item['count'])
-    })
+function getMaxCount(obj){
+    let result=[]
+    for(let item in obj){
+        result.push(obj[item])
+    }
     let max = result.reduce(function(a, b) {
         return Math.max(a, b);
     });
     return max
+
 }
 
-function getMaxResult(collection,max){
-    let result=[];
-    collection.forEach((item)=>{
-        if(item['count']==max){
-            result.push(item)
+function getMaxResult(obj,max){
+    let result={};
+    for(let item in obj){
+        if(obj[item]==max){
+            result[item]=max
         }
-    })
+    }
     return result
+
+
 }
 
 function main(str){
